6c73341cefe816a4544785bdd361404d3aa35567
Jeniffer schurhaus Lucas <jenifferlucas2019@gmail.com>
2026-07-28 19:24:03 -0300

    Refactor employee management functions in script.js

--- segurança do trabalho/script.js
+++ segurança do trabalho/script.js
@@ -14,129 +14,118 @@
 
 // console.log(dino);
 
-let funcionarios = []
+let funcionarios = [];
 
-function salvarDados(){
-    localStorage.setItem('funcionarios', JSON.stringify(funcionarios))
-    
-    // let texto = JSON.stringify(dinos)
-    // localStorage.setItem('dinos', texto)
+function salvarDados() {
+    localStorage.setItem("funcionarios", JSON.stringify(funcionarios));
 }
 
-function carregarDados(){
-    funcionarios = JSON.parse(localStorage.getItem('funcionarios')) || []
-    
-    // let textoLido = localStorage.getItem('dinos')
-    // dinos = JSON.parse(textoLido)
+function carregarDados() {
+    funcionarios = JSON.parse(localStorage.getItem("funcionarios")) || [];
 }
 
-function cadastrarfuncionario() {
+function cadastrarFuncionario() {
 
-    carregarDados()
+    carregarDados();
 
     const funcionario = {
-        nome: document.getElementById('input-nome').value,
-        sertor: Number(document.getElementById("input-setor").value),
+        id: Date.now(),
+        nome: document.getElementById("input-nome").value,
+        setor: document.getElementById("input-setor").value,
         funcao: document.getElementById("input-funcao").value,
-        data: Number(document.getElementById("input-data").value),
-        validade: Number(document.getElementById('input-validade').value),
+        data: document.getElementById("input-data").value,
+        validade: document.getElementById("input-validade").value
+    };
+
+    funcionarios.push(funcionario);
+
+    salvarDados();
+    mostrarFuncionarios();
+    limparFormulario();
+}
+
+function mostrarFuncionarios() {
+
+    carregarDados();
+
+    let painel = document.getElementById("painel-funcionario");
+
+    painel.innerHTML = "";
+
+    for (let i = 0; i < funcionarios.length; i++) {
+
+        painel.innerHTML += `
+        <div class="card">
+            <h2>${funcionarios[i].nome}</h2>
+
+            <p><b>Setor:</b> ${funcionarios[i].setor}</p>
+
+            <p><b>Função:</b> ${funcionarios[i].funcao}</p>
+
+            <p><b>Data:</b> ${funcionarios[i].data}</p>
+
+            <p><b>Validade:</b> ${funcionarios[i].validade}</p>
+        </div>
+        `;
     }
-  
-
-    console.log(funcionario);
-
-    limparFormulario()
-    mostrarTodos()
-
-    salvarDados()
 }
 
-function limparformulario() {
-    document.getElementById('input-nome').value = ''
-    document.getElementById('input-setor').value = ''
-    document.getElementById('input-funcao').value = ''
-    document.getElementById('input-data').value = ''
-    document.getElementById('input-validade').value = ''
+function pesquisarFuncionario() {
 
-    document.getElementById('input-nome').focus()
-}
+    carregarDados();
 
-function mostrarformulario(){
-    document.getElementById('painel-funcionario').innerHTML = '' 
+    let nome = document.getElementById("input-nome").value;
 
-    for(let i=0; i<segurancadotrabalho.length; i++){
-        // alert(dinos[i].nome)
-        document.getElementById('painel-funcionario').innerHTML += 
-        `<div class="card-dino">
-            <h2>${funcionario[i].nome}</h2>
-            <p>nome: ${funcionario[i].setor}</p>
-            <p>validade:${funçao$funcionario[i].funçao}</p>
-            <p>:validade ${funcionario[i].validade}</p>
-            <p>${funcionario[i].id}</p>
-        </div>` 
-    }
+    for (let i = 0; i < funcionarios.length; i++) {
 
-}
+        if (funcionarios[i].nome.toLowerCase() == nome.toLowerCase()) {
 
-function salvarfuncionario() {
-    
-    // window.location.href = 'teste.html'
+            document.getElementById("input-setor").value = funcionarios[i].setor;
+            document.getElementById("input-funcao").value = funcionarios[i].funcao;
+            document.getElementById("input-data").value = funcionarios[i].data;
+            document.getElementById("input-validade").value = funcionarios[i].validade;
 
-    carregarDados()
-
-
-
- 
-    console.log(funcionario);
-
-}
-
-function segurancadotrabalho(){
-    let nomeProcurado = document.getElementById('input-nome').value
-
-    for(let i = 0; i<segurancadotrabalho.length; i++){
-        if(nomeProcurado == segurancadotrabalho[i].nome ){
-            console.log(segurancadotrabalho[i]);
-            document.getElementById('input-nome').value = funcionario[i].nome
-            document.getElementById('input-setor').value = funcionario[i].setor
-            document.getElementById('input-funcao').value = funcionario[i].funçao
-            document.getElementById('input-validade').value = funcionario[i].validade
-            console.log(i);
-        }
-        
-    }
-
-}
-
-function salvarfuncionario(){
-    let id = Number(document.getElementById('input-nome').value)
-
-    for (let i = 0; i< mostrarfuncionario.length; i++)
-    {
-        if(id == mostrarfuncionario[i].nome ){
-          console.log(segurancadotrabalho[i]);
-            document.getElementById('input-nome').value = funcionario[i].nome
-            document.getElementById('input-setor').value = funcionario[i].setor
-            document.getElementById('input-funcao').value = funcionario[i].funçao
-            document.getElementById('input-validade').value = funcionario[i].validade
-            console.log(i);
+            return;
         }
     }
-    
-    mostrarformulario()
-    limparformulario()
 
+    alert("Funcionário não encontrado!");
 }
-function excluirfuncionario(){
-    let id = Number(document.getElementById('input-validade').value)
 
-    for(let i = 0; i<dinos.length; i++){
-        if(id == funcionario[i].id ){
-            console.log(funcionario[i]);
-            funcionario.splice(i, 1)
-            console.log(i);
+function excluirFuncionario() {
+
+    carregarDados();
+
+    let nome = document.getElementById("input-nome").value;
+
+    for (let i = 0; i < funcionarios.length; i++) {
+
+        if (funcionarios[i].nome.toLowerCase() == nome.toLowerCase()) {
+
+            funcionarios.splice(i, 1);
+
+            salvarDados();
+            mostrarFuncionarios();
+            limparFormulario();
+
+            alert("Funcionário excluído!");
+
+            return;
         }
     }
-    mostrarformulario()
-    limparformulario()
+
+    alert("Funcionário não encontrado!");
 }
+
+function limparFormulario() {
+
+    document.getElementById("input-nome").value = "";
+    document.getElementById("input-setor").value = "";
+    document.getElementById("input-funcao").value = "";
+    document.getElementById("input-data").value = "";
+    document.getElementById("input-validade").value = "";
+
+    document.getElementById("input-nome").focus();
+}
+
+mostrarFuncionarios();
